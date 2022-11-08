e812bee0985da2b9fd35f9022672e14c324a1e5b
yunimm <yuu9916@gmail.com>
2022-11-08 20:47:45 +0800

    add shot1 layout

---
+++ src/components/AllGif.jsx
@@ -0,0 +1,4 @@
+import et from '../assets/GIF/et.gif';
+import elf from '../assets/GIF/et.gif';
+
+export { et, elf };
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,68 +1,28 @@
-import { useLayoutEffect, useRef } from 'react';
-// import logo from '../assets/logo.svg';
 import './App.css';
-import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-gsap.registerPlugin(ScrollTrigger);
-const Box = ({ children, className }) => {
-  return <div className={'box ' + className}>{children}</div>;
-};
-
-function Container({ children }) {
-  return (
-    <div>
-      <Box>Don't Animate Me</Box>
-    </div>
-  );
-}
+import { sun, LOGO_main, LOGO_header, cloud_sm, cloud_xl, ufo } from './AllPng';
+import { et, elf } from './AllGif';
 function App() {
-  const comp = useRef();
-
-  // useLayoutEffect =
-  //   (() => {
-  //     //animation here
-  //     let ctx = gsap.context(() => {
-  //       gsap.to('.box', { rotation: '+=360' });
-  //     }, comp);
-
-  //     return () => ctx.revert();
-  //   },
-  //   []);
-  const app = useRef();
-
-  useLayoutEffect(() => {
-    const ctx = gsap.context(() => {
-      // Target the two specific elements we have asigned the animate class
-      gsap.to('.animate', {
-        x: 100,
-        repeat: -1,
-        repeatDelay: 1,
-        yoyo: true,
-      });
-      gsap.to('.animate2', {
-        x: 150,
-        repeat: -1,
-        repeatDelay: 1,
-        yoyo: true,
-      });
-      gsap.to('.animate3', {
-        scrollTrigger: {
-          trigger: '.animate3',
-          toggleActions: 'restart none none none',
-        },
-        x: 400,
-      });
-    }, app); // <- Scope!
-
-    return () => ctx.revert();
-  }, []);
   return (
     <div className="App">
-      <div className="app" ref={app}>
-        <Box className="animate">Box</Box>
-        <Container></Container>
-        <Box className="animate2">Box</Box>
-        <Box className="animate3">Box3</Box>
+      <div id="bg">
+        <div className="px-10 flex items-center justify-between" id="header">
+          <img className="" src={LOGO_header} alt="logo on screen" />
+          <div className="text-white tex-xl flex gap-8">
+            <a href="http://">關卡資訊</a>
+            <a href="http://">活動說明</a>
+            <a href="http://">獎項設計</a>
+          </div>
+        </div>
+        <img id="sun" src={sun} alt="sun on screen" />
+        <img id="logo_main" src={LOGO_main} alt="logo on screen" />
+        <img id="cloud_sm" src={cloud_sm} alt="cloud on screen" />
+        <img id="cloud_xl" src={cloud_xl} alt="cloud on screen" />
+        <div id="ufo_wrapper">
+          <div className="relative">
+            <img id="ufo" src={ufo} alt="ufo on screen" />
+            <img id="et" src={et} alt="et on screen" />
+          </div>
+        </div>
       </div>
     </div>
   );
