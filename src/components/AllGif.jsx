77907fd28d301e821916e8a02d8d94c23e535cae
yunimm <yuu9916@gmail.com>
2022-11-09 22:21:16 +0800

    fix Header, add Header hover animation

--- src/components/AllGif.jsx
+++ src/components/AllGif.jsx
@@ -1,4 +1,4 @@
 import et from '../assets/GIF/et.gif';
-import elf from '../assets/GIF/et.gif';
+import elf from '../assets/GIF/elf.gif';
 
 export { et, elf };
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,31 @@
-import './App.css';
-import { sun, LOGO_main, LOGO_header, cloud_sm, cloud_xl, ufo } from './AllPng';
+import './App.scss';
+import Header from './Header';
+import { sun, LOGO_main, cloud_sm, cloud_xl, ufo } from './AllPng';
 import { et, elf } from './AllGif';
 function App() {
   return (
     <div className="App">
       <div id="bg">
-        <div className="px-10 flex items-center justify-between" id="header">
-          <img className="" src={LOGO_header} alt="logo on screen" />
-          <div className="text-white tex-xl flex gap-8">
-            <a href="http://">關卡資訊</a>
-            <a href="http://">活動說明</a>
-            <a href="http://">獎項設計</a>
-          </div>
-        </div>
+        <Header />
         <img id="sun" src={sun} alt="sun on screen" />
         <img id="logo_main" src={LOGO_main} alt="logo on screen" />
         <img id="cloud_sm" src={cloud_sm} alt="cloud on screen" />
         <img id="cloud_xl" src={cloud_xl} alt="cloud on screen" />
+
         <div id="ufo_wrapper">
           <div className="relative">
             <img id="ufo" src={ufo} alt="ufo on screen" />
             <img id="et" src={et} alt="et on screen" />
           </div>
         </div>
-        <button id="button" />
+
+        {/* end */}
+        <div>
+          <button type="button" id="button_sm" />
+        </div>
+        <footer id="footer">
+          <span>Copyright ©2022 炙燒三重奏.All rights reserved.</span>
+        </footer>
       </div>
     </div>
   );
